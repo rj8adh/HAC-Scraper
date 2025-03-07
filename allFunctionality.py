494dd7f33134c97719f4d338aadc4b6690a62684
rj8 <rj8adhikari@gmail.com>
2025-03-06 22:24:02 -0600

    Added Calculate Average Function For Future Add Grade Method

--- allFunctionality.py
+++ allFunctionality.py
@@ -1,4 +1,4 @@
-# Testing usage
+# TODO Fix bug where certain grading categories dont show up (Primarily in IED)
 from sessionStarter import login
 from dataParsers import *
 from pageScrapers import *
@@ -18,14 +18,14 @@ tranGrades = getTranscriptGrades(transcript)
 # classAssignmentIDs = getAssignmentPageIDs(homeSC) # No longer useful for new assignment scraping strategy
 allAssignmentData = getAssignmentsForClass(session)
 classAssignments = parseAssignments(allAssignmentData)
+
 for className in classAssignments:
     print(f"\n\nASSIGNMENTS FOR {className}:\n\n")
-    for detail in classAssignments[className]:
-        for detailData in classAssignments[className][detail]:
-            for data in detailData:
-                print(f"{data}: {detailData[data]}")
-            print()
-
+    print(calcAverage(classAssignments[className]['assignments'], classAssignments[className]['categories']))
+    # for assignmentData in classAssignments[className]['assignments']:
+    #     for data in assignmentData:
+    #         print(f"{data}: {assignmentData[data]}")
+    #     print()
 
 # print(getAssignmentsForClass(session, "2353121", "1", "3"))
 tranGrades = modifyTranGrades(tranGrades, {'CHEM':'0'})
